67145f5e62d46d8fef55c745c8e92222560297a1
Dell <Dell@DESKTOP-1674E11>
2022-11-22 17:27:39 +0530

    First commit

---
+++ src/main/java/com/EclipseToGit/EclipseToGit1Application.java
@@ -0,0 +1,14 @@
+package com.EclipseToGit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EclipseToGit1Application {
+
+	public static void main(String[] args) {
+		System.out.println("***************    Ayyappa swamy ");
+		SpringApplication.run(EclipseToGit1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/EclipseToGit/EclipseToGit1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.EclipseToGit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EclipseToGit1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
